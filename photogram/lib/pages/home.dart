597dd4a6ce4fedd2bf1f3bf1bb5fc3b75d063e44
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2019-09-30 22:39:32 +0200

    add appbar and botton nav bar

--- photogram/lib/pages/home.dart
+++ photogram/lib/pages/home.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:photogram/pages/activity_feed.dart';
+import 'package:photogram/pages/profile.dart';
+import 'package:photogram/pages/search.dart';
+import 'package:photogram/pages/timeline.dart';
+import 'package:photogram/pages/upload.dart';
 
 final GoogleSignIn googleSignin = GoogleSignIn();
 
@@ -11,10 +16,15 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   bool isAuth = false;
+  PageController pageController;
+  int pageIndex = 0;
 
   @override
   void initState() {
     super.initState();
+
+    pageController = PageController();
+
     googleSignin.onCurrentUserChanged.listen(
       handleSignInSuccess,
       onError: handleSignInError,
@@ -45,6 +55,12 @@ class _HomeState extends State<Home> {
     });
   }
 
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   logout() {
     googleSignin.signOut();
   }
@@ -54,10 +70,57 @@ class _HomeState extends State<Home> {
   }
 
   Widget buildAuthScreen() {
-    return RaisedButton(
-      child: Text('Logout'),
-      onPressed: logout,
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[
+          Timeline(),
+          ActivityFeed(),
+          Upload(),
+          Search(),
+          Profile(),
+        ],
+        controller: pageController,
+        onPageChanged: onPageChange,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: pageIndex,
+        onTap: onTap,
+        activeColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.whatshot),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_active),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.photo_camera,
+              size: 35.0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+      ),
     );
+  }
+
+  onTap(int pageIndex) {
+    pageController.jumpToPage(
+      pageIndex,
+    );
+  }
+
+  onPageChange(int pageIndex) {
+    setState(() {
+      this.pageIndex = pageIndex;
+    });
   }
 
   Widget buildUnAuthScreen() {
