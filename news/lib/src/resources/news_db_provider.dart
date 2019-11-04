adfc33d5e03877f49958a1754a108e0d27fe93e0
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2019-11-04 23:22:31 +0100

    create db

---
+++ news/lib/src/resources/news_db_provider.dart
@@ -0,0 +1,39 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import 'package:news/src/models/item_model.dart';
+
+class NewsDbProvider {
+  Database db;
+
+  init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, "items.db");
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+        CREATE TABLE Items
+          (
+            id INTEGER PRIMARY KEY,
+            type TEXT,
+            by TEXT,
+            time INTEGER,
+            text TEXT,
+            parent INTEGER,
+            kids BLOB,
+            dead INTEGER,
+            deleted INTEGER,
+            url TEXT,
+            score INTEGER,
+            title TEXT,
+            descendants INTEGER
+          );
+        """);
+      }
+    );
+  }
+}
