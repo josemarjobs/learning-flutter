5b2ac4d526cad98b1abf302f4c53604913cfea36
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2021-07-12 21:02:29 -0700

    add spotify desktop and web ui

---
+++ flutter_spotify_ui/lib/widgets/playlist_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spotify_ui/data/data.dart';
+import 'package:flutter_spotify_ui/widgets/widgets.dart';
+
+class PlaylistScreen extends StatefulWidget {
+  final Playlist playlist;
+  PlaylistScreen({
+    Key? key,
+    required this.playlist,
+  }) : super(key: key);
+
+  @override
+  _PlaylistScreenState createState() => _PlaylistScreenState();
+}
+
+class _PlaylistScreenState extends State<PlaylistScreen> {
+  ScrollController? _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 140.0,
+        leading: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                customBorder: const CircleBorder(),
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(6.0),
+                  decoration: const BoxDecoration(
+                    color: Colors.black26,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(Icons.chevron_left, size: 28.0),
+                ),
+              ),
+              const SizedBox(width: 16.0),
+              InkWell(
+                customBorder: const CircleBorder(),
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(6.0),
+                  decoration: const BoxDecoration(
+                    color: Colors.black26,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(Icons.chevron_right, size: 28.0),
+                ),
+              )
+            ],
+          ),
+        ),
+        actions: [
+          TextButton.icon(
+            style: TextButton.styleFrom(
+              primary: Theme.of(context).iconTheme.color,
+            ),
+            onPressed: () {},
+            icon: Icon(Icons.account_circle_outlined, size: 30.0),
+            label: Text('Peter Griffin'),
+          ),
+          SizedBox(width: 8.0),
+          IconButton(
+            padding: const EdgeInsets.only(),
+            onPressed: () {},
+            icon: const Icon(Icons.keyboard_arrow_down, size: 30.0),
+          ),
+          SizedBox(width: 20.0),
+        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              const Color(0xFFAF1018),
+              Theme.of(context).backgroundColor
+            ],
+            stops: [0, 0.3],
+          ),
+        ),
+        child: Scrollbar(
+          isAlwaysShown: true,
+          controller: _scrollController,
+          child: ListView(
+            controller: _scrollController,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 60.0,
+            ),
+            children: [
+              PlaylistHeader(playlist: widget.playlist),
+              TracksList(tracks: widget.playlist.songs),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_spotify_ui/lib/widgets/widgets.dart
@@ -0,0 +1,5 @@
+export 'side_menu.dart';
+export 'playlist_screen.dart';
+export 'playlist_header.dart';
+export 'tracks_list.dart';
+export 'current_track.dart';
